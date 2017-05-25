85d27d14a89c6b60b1b6361d089fe3b66ec655f3
Kimpe Andy <andykimpe@gmail.com>
2017-05-25 17:32:33 +0200

    Update genupdate.sh

--- genupdate.sh
+++ genupdate.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env bash
 cd /root
-rm -f gen.sh
-wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh
-chmod +x gen.sh
-/root/gen.sh
+bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh)
