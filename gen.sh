a25976bb376b80086989e0e4647290723ba660ec
Kimpe Andy <andykimpe@gmail.com>
2017-05-25 17:33:28 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -7,9 +7,7 @@ cp /root/euroiptv-epg-fr/logo/* /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
 rm -rf /root/euroiptv-epg-fr/ /root/config.txt /root/euroiptv.xml /root/sed.sh
 wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config.txt
 tv_grab_fr_telerama --config-file config.txt --output /root/euroiptv.xml --days 14
-wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/sed.sh
-chmod +x sed.sh
-/root/sed.sh
+bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/sed.sh)
 rm -rf /home/xtreamcodes/xmltv/
 mkdir -p /home/xtreamcodes/xmltv/
 cp /root/euroiptv.xml /home/xtreamcodes/xmltv/guide.xml
