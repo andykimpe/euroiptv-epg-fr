4eada62dd06c06939226ed39006cdad5876dca99
Kimpe Andy <andykimpe@gmail.com>
2017-05-25 17:22:29 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -3,7 +3,7 @@ cd /root
 git clone https://github.com/andykimpe/euroiptv-epg-fr.git
 rm -rf /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
 mkdir -p /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
-cp /root/euroiptv-epg-fr/logo* /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
+cp /root/euroiptv-epg-fr/logo/* /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
 rm -rf /root/euroiptv-epg-fr/ /root/config.txt /root/euroiptv.xml /root/sed.sh
 wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config.txt
 tv_grab_fr_telerama --config-file config.txt --output /root/euroiptv.xml --days 14
