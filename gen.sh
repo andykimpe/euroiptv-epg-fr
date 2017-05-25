4f5359ab25f0731bb552cfe09e12084dea6268ff
Kimpe Andy <andykimpe@gmail.com>
2017-05-25 16:54:28 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,1 +1,7 @@
 #!/usr/bin/env bash
+cd /root
+git clone https://github.com/andykimpe/euroiptv-epg-fr.git
+rm -rf /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
+mkdir -p /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
+cp /root/euroiptv-epg-fr/logo* /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
+rm -rf /root/euroiptv-epg-fr/
