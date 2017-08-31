1159fce24ca7779f56d1f76d1569de363f60f6ed
Kimpe Andy <andykimpe@gmail.com>
2017-09-01 00:51:04 +0200

    Update sed2.sh

--- sed2.sh
+++ sed2.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sed -i 's|<display-name>TF1.mycanal.fr</display-name>|<display-name>TF1</display-name>|' "/root/euroiptv.xml"
-#sed -i 's|channel="TF1.mycanal.fr"|channel="TF1"|' "/root/euroiptv.xml"
+sed -i 's|channel="TF1.mycanal.fr"|channel="TF1"|' "/root/euroiptv.xml"
 sed -i 's|TF1.mycanal.fr|TF1.fr|' "/root/euroiptv.xml"
