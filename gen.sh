fe0debd216dec237c92fa09a1f446e4c7a5f270f
Kimpe Andy <andykimpe@gmail.com>
2017-09-01 00:26:26 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -3,10 +3,11 @@ cd /root
 git clone https://github.com/andykimpe/euroiptv-epg-fr.git
 mkdir -p /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
 cp -f /root/euroiptv-epg-fr/logo/* /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
-rm -rf /root/euroiptv-epg-fr/ /root/config.txt /root/euroiptv.xml /root/sed.sh
-wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config.txt
-tv_grab_fr_telerama --config-file config.txt --output /root/euroiptv.xml --days 11
-bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/sed.sh)
+rm -rf /root/euroiptv-epg-fr/ /root/config.txt /root/euroiptv.xml /root/sed.sh 
+#wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config.txt
+#tv_grab_fr_telerama --config-file config.txt --output /root/euroiptv.xml --days 11
+wget "http://racacaxtv.ga/xmltv.php" -O "/root/euroiptv.xml"
+bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/sed2.sh)
 rm -rf /home/xtreamcodes/xmltv/
 mkdir -p /home/xtreamcodes/xmltv/
 cp /root/euroiptv.xml /home/xtreamcodes/xmltv/guide.xml
