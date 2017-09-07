adb22d6ad7d7c666a5f85d616b5317859df3f973
Kimpe Andy <andykimpe@gmail.com>
2017-09-07 15:11:52 +0200

    Update genupdate.sh

--- genupdate.sh
+++ genupdate.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /root
-bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh)
+bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen2.sh)
