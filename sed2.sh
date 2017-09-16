669835c677e72172d88f3e43258d51a38703bdb1
Kimpe Andy <andykimpe@gmail.com>
2017-09-16 23:07:56 +0200

    Delete sed2.sh

--- sed2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#sed -i 's|<display-name>TF1.mycanal.fr</display-name>|<display-name>TF1</display-name>|' "/root/euroiptv.xml"
-#sed -i 's|channel="TF1.mycanal.fr"|channel="TF1"|' "/root/euroiptv.xml"
-sed -i 's|TF1.mycanal.fr|TF1.fr|' "/root/euroiptv.xml"
-sed -i 's|FRANCE 2.mycanal.fr|France2.fr|' "/root/euroiptv.xml"
-sed -i 's|FRANCE 3.mycanal.fr|France3.fr|' "/root/euroiptv.xml"
