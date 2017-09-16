cc41187b21fd142f66cdd980f8dcd3214564312b
Kimpe Andy <andykimpe@gmail.com>
2017-09-17 00:09:07 +0200

    Update France2update.sh

--- config/France/France2/France2update.sh
+++ config/France/France2/France2update.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 cd /root
-git clone https://github.com/andykimpe/euroiptv-epg-fr.git
-mkdir -p /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
-cp -f /root/euroiptv-epg-fr/logo/* /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
 cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France2
-rm -rf "WebGrab++.config.xml" "guide.xml" "/root/euroiptv-epg-fr" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France2/France2.xml.gz
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France2/France2.xml" -O "WebGrab++.config.xml"
+rm -rf "WebGrab++.config.xml" "guide.xml" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.gz
+wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France2/France2.xml" -O "WebGrab++.config.xml"
 ./run.sh
-gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France2/France2.xml
-chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France2/*
-chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France2/*
+gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml
+chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/*
+chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/*
 /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg.php
