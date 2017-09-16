51062d5c801a84d80a5f0d1089dca1ac6cc5f7e1
Kimpe Andy <andykimpe@gmail.com>
2017-09-17 00:00:17 +0200

    Update TF1update.sh

--- config/France/TF1/TF1update.sh
+++ config/France/TF1/TF1update.sh
@@ -3,11 +3,11 @@ cd /root
 git clone https://github.com/andykimpe/euroiptv-epg-fr.git
 mkdir -p /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
 cp -f /root/euroiptv-epg-fr/logo/* /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
-cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/TF1
-rm -rf "WebGrab++.config.xml" "guide.xml" "/root/euroiptv-epg-fr" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/TF1/TF1.xml.gz
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/TF1/TF1.xml" -O "WebGrab++.config.xml"
+cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1
+rm -rf "WebGrab++.config.xml" "guide.xml" "/root/euroiptv-epg-fr" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml.gz
+wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1.xml" -O "WebGrab++.config.xml"
 ./run.sh
-gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/TF1/TF1.xml
-chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/TF1/*
-chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/TF1/*
+gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml
+chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/*
+chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/*
 /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg.php
