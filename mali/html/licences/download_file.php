55218df8ea6975296db591b8686fe72fc0a838ac
andy <andy@andy-Revo-One-RL85.numericable.fr>
2017-09-17 00:51:07 +0200

    add mali

---
+++ mali/html/licences/download_file.php
@@ -0,0 +1,17 @@
+<?php
+file_put_contents("/tmp/d2",print_r($_POST,1));
+
+$file = 'iptv_panel.tar.gz';
+
+if (file_exists($file)) {
+    header('Content-Description: File Transfer');
+    header('Content-Type: application/octet-stream');
+    header('Content-Disposition: attachment; filename="'.basename($file).'"');
+    header('Expires: 0');
+    header('Cache-Control: must-revalidate');
+    header('Pragma: public');
+    header('Content-Length: ' . filesize($file));
+    readfile($file);
+    exit;
+}
+?>
