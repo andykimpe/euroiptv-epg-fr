0d431524446de4ad11f140c654c32addfb75d278
Kimpe Andy <andykimpe@gmail.com>
2017-09-17 00:03:40 +0200

    Update install.sh

--- config/France/France2/install.sh
+++ config/France/France2/install.sh
@@ -21,16 +21,16 @@ wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/ma
 wget http://webgrabplus.com/sites/default/files/download/SW/V2.1.0/WebGrabPlus_V2.1_install.tar.gz
 tar -xvf WebGrabPlus_V2.1_install.tar.gz
 rm -f WebGrabPlus_V2.1_install.tar.gz
-mv .wg++ TF1
-cd TF1
+mv .wg++ France2
+cd France2
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
 ./install.sh
 rm -f "siteini.user/France/telerama.fr.channels.xml" "siteini.user/France/telerama.fr.ini"
 mkdir -p "siteini.user/France"
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/telerama.fr_.channels.xml" -O "siteini.user/France/telerama.fr.channels.xml"
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/telerama.fr_.ini" -O "siteini.user/France/telerama.fr.ini"
-wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/TF1/TF1.sh -O /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/TF1.sh
-chmod +x /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/TF1.sh
-if ! grep -q "00 05 * * * root /bin/bash /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/TF1.sh" /etc/crontab; then
-    echo "00 05 * * * root /bin/bash /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/TF1.sh" >> /etc/crontab;
+wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France2/France2.sh -O /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France.sh
+chmod +x /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France.sh
+if ! grep -q "00 05 * * * root /bin/bash /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France.sh" /etc/crontab; then
+    echo "00 05 * * * root /bin/bash /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France.sh" >> /etc/crontab;
 fi
