642b3c0a7371f7a7ef363ff225e83cac446666e6
Kimpe Andy <andykimpe@gmail.com>
2017-09-17 00:01:20 +0200

    Update TF1update.sh

--- config/France/TF1/TF1update.sh
+++ config/France/TF1/TF1update.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 cd /root
-git clone https://github.com/andykimpe/euroiptv-epg-fr.git
-mkdir -p /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
-cp -f /root/euroiptv-epg-fr/logo/* /home/xtreamcodes/iptv_xtream_codes/wwwdir/logo/
 cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1
 rm -rf "WebGrab++.config.xml" "guide.xml" "/root/euroiptv-epg-fr" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml.gz
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1.xml" -O "WebGrab++.config.xml"
