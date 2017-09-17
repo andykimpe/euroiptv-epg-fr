5994c28dbeb5d67f6102f85cc544cd97d0503f02
andy <andy@andy-Revo-One-RL85.numericable.fr>
2017-09-17 13:10:33 +0200

    update

--- config/France/C8/C8.sh
+++ config/France/C8/C8.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 cd /root
-rm -f ""C8"update.sh"
-wget "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/C8/"C8"update.sh"
-chmod +x  ""C8"update.sh"
-""C8"update.sh"
-rm -f ""C8"update.sh"
+bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/C8/C8update.sh)
