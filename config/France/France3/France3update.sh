178c75e7068d39fe44851cf33f8f3c9eb0da0315
Kimpe Andy <andykimpe@gmail.com>
2017-09-17 03:45:53 +0200

    Update France3update.sh

--- config/France/France3/France3update.sh
+++ config/France/France3/France3update.sh
@@ -7,4 +7,9 @@ wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/ma
 gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml
 chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/*
 chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/*
+if [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
+then
+/home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg.php
+elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php ]  
 /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php
+fi
