5f67c5b8cf6ce8ac3e866666f514bac781b456dd
Kimpe Andy <andykimpe@gmail.com>
2017-09-17 06:17:03 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -87,3 +87,4 @@ bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/confi
 bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France2/install.sh)
 bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France3/install.sh)
 bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/CanalPlus/install.sh)
+bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France5/install.sh)
