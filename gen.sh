a722ffef54bbaa594f3e6e820b09dbaa2b35dcd5
Kimpe Andy <andykimpe@gmail.com>
2017-09-17 18:23:35 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -4,7 +4,7 @@ cat > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <tv generator-info-name="WebGrab+Plus/w MDB &amp; REX Postprocess -- version  V2.1 -- Jan van Straaten" generator-info-url="http://www.webgrabplus.com">
   <channel id="TF1.fr">
-    <display-name lang="fr">tF1</display-name>
+    <display-name lang="fr">TF1</display-name>
     <icon src="https://github.com/andykimpe/euroiptv-epg-fr/raw/master/logo/France/tf1.png" />
     <url>http://www.tv.orange.fr</url>
   </channel>
