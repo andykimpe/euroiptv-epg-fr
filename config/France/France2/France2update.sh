b89256c805fc6f9c529bcef35702efe9dff5f274
Kimpe Andy <andykimpe@gmail.com>
2017-09-17 02:32:44 +0200

    Update France2update.sh

--- config/France/France2/France2update.sh
+++ config/France/France2/France2update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /root
-cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France2
+cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2
 rm -rf "WebGrab++.config.xml" "guide.xml" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.gz
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France2/France2.xml" -O "WebGrab++.config.xml"
 ./run.sh
