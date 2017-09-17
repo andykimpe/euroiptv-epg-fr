cf6c8073ed7d7382e1126fd49c309fa99ac48c10
Kimpe Andy <andykimpe@gmail.com>
2017-09-17 03:08:13 +0200

    Update install.sh

--- config/France/TF1/install.sh
+++ config/France/TF1/install.sh
@@ -34,3 +34,4 @@ chmod +x /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.sh
 if ! grep -q "00 05 * * * root /bin/bash /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.sh" /etc/crontab; then
     echo "00 05 * * * root /bin/bash /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.sh" >> /etc/crontab;
 fi
+/home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.sh
