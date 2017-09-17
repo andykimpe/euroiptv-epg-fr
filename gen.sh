dff575690c02ff8ccaf7c2fb7923bf133cf576de
Kimpe Andy <andykimpe@gmail.com>
2017-09-17 18:29:54 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 cd /root
+rm -f /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml
 cat > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml <<EOF
 <?xml version="1.0" encoding="UTF-8"?>
 <tv generator-info-name="WebGrab+Plus/w MDB &amp; REX Postprocess -- version  V2.1 -- Jan van Straaten" generator-info-url="http://www.webgrabplus.com">
