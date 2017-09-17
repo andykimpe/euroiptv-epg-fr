b7ced5e70e003d9e309ff43402347d70e55fff83
Kimpe Andy <andykimpe@gmail.com>
2017-09-17 04:32:50 +0200

    Update CanalPlusupdate.sh

--- config/France/CanalPlus/CanalPlusupdate.sh
+++ config/France/CanalPlus/CanalPlusupdate.sh
@@ -10,6 +10,7 @@ chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/F
 if [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
 then
 /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg.php
-elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php ]  
+elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php ]
+then
 /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php
 fi
