ab52018d39a5084d9eef903a6abfd40e48aca251
Kimpe Andy <andykimpe@gmail.com>
2017-09-17 04:22:45 +0200

    Update CanalPlusupdate.sh

--- config/France/CanalPlus/CanalPlusupdate.sh
+++ config/France/CanalPlus/CanalPlusupdate.sh
@@ -1,1 +1,15 @@
-
+#!/bin/bash
+cd /root
+cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/CanalPlus
+rm -rf "WebGrab++.config.xml" "guide.xml" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/CanalPlus/CanalPlus.xml.gz
+wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/CanalPlus/CanalPlus.xml" -O "WebGrab++.config.xml"
+./run.sh
+gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/CanalPlus/CanalPlus.xml
+chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/CanalPlus/*
+chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/CanalPlus/*
+if [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
+then
+/home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg.php
+elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php ]  
+/home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php
+fi
