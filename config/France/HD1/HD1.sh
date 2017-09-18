775d2d27f4378e5dafefb329fcb5a779734278cf
andy <andy@andy-Revo-One-RL85.numericable.fr>
2017-09-18 02:27:28 +0200

    update

---
+++ config/France/HD1/HD1.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /root
+bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/HD1/HD1update.sh)
---
+++ config/France/HD1/HD1update.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+cd /root
+cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1
+rm -rf "WebGrab++.config.xml" "guide.xml" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.gz
+wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/HD1/HD1.xml" -O "WebGrab++.config.xml"
+rm -f /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml
+./run.sh
+cp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.save
+gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml
+mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.save /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml
+sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml
+sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml
+sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml
+sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml
+sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml
+sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml
+sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml
+head -n -1 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/HD1.xml
+chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/*
+chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/HD1/*
+if [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
+then
+/home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg.php
+elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php ]
+then
+/home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php
+fi
+bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh)
