8cf6ce39a177997c8707c4ad70a776f7e1fba00f
Kimpe Andy <andykimpe@gmail.com>
2017-10-05 13:52:51 +0200

    Update unistall.sh

--- unistall.sh
+++ unistall.sh
@@ -2,4 +2,4 @@
 rm -f /etc/crontab
 mv /etc/crontab.xtreamcodesinstallepg /etc/crontab
 service cron restart
-rm -rf rm -rf /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/
+rm -rf rm -rf /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/ /usr/bin/purgeiptables
