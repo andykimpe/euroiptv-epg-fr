0a7352efec69563aca64fddd3cca2a3e1cc7f0f4
Kimpe Andy <andykimpe@gmail.com>
2017-10-05 13:55:21 +0200

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -7,7 +7,7 @@ cat > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml <<EOF
 EOF
 cat >> /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml <<EOF
   <channel id="TF1.fr">
-    <display-name lang="fr">TF1</display-name>
+    <display-name lang="fr">FR TF1</display-name>
     <icon src="https://github.com/andykimpe/euroiptv-epg-fr/raw/master/logo/France/tf1.png" />
     <url>http://tv.orange.fr</url>
   </channel>
@@ -15,7 +15,7 @@ EOF
 cat /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml >> /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml
 cat >> /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml <<EOF
   <channel id="France2.fr">
-    <display-name lang="fr">France 2</display-name>
+    <display-name lang="fr"> FR France 2</display-name>
     <icon src="https://github.com/andykimpe/euroiptv-epg-fr/raw/master/logo/France/france2.png" />
     <url>http://tv.orange.fr</url>
   </channel>
@@ -23,12 +23,20 @@ EOF
 cat /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml >> /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml
 cat >> /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml <<EOF
   <channel id="France3.fr">
-    <display-name lang="fr">France 3</display-name>
+    <display-name lang="fr">FR France 3</display-name>
     <icon src="https://github.com/andykimpe/euroiptv-epg-fr/raw/master/logo/France/france3.png" />
     <url>http://tv.orange.fr</url>
   </channel>
 EOF
 cat /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml >> /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml
+cat >> /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml <<EOF
+  <channel id="CANALplus.fr">
+    <display-name lang="fr">FR Canal+</display-name>
+    <icon src="https://github.com/andykimpe/euroiptv-epg-fr/raw/master/logo/France/canal%2B.png" />
+    <url>http://tv.orange.fr</url>
+  </channel>
+EOF
+cat /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/CanalPlus/CanalPlus.xml >> /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml
 cp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml.save
 rm -f /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml.gz
 gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml
