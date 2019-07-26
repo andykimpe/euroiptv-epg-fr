0cd5c8ee10a84689ebd7ace6157f046ccbe96b5c
Kimpe Andy <andykimpe@gmail.com>
2019-07-26 20:18:34 +0200

    Update TF1.sh

--- config/France/TF1/TF1.sh
+++ config/France/TF1/TF1.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /root
-bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1update.sh)
+bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1update.sh)
