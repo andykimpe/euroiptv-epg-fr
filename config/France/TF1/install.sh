180e0541f46d0719234128cb8ebb2688bf7daf9a
Kimpe Andy <andykimpe@gmail.com>
2019-07-26 23:11:14 +0200

    Update install.sh

--- config/France/TF1/install.sh
+++ config/France/TF1/install.sh
@@ -16,18 +16,26 @@ cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
 cd France
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
-wget http://webgrabplus.com/sites/default/files/download/SW/V2.1.0/WebGrabPlus_V2.1_install.tar.gz
-tar -xvf WebGrabPlus_V2.1_install.tar.gz
-rm -f WebGrabPlus_V2.1_install.tar.gz
+# stable
+#wget http://webgrabplus.com/sites/default/files/download/SW/V2.1.0/WebGrabPlus_V2.1_install.tar.gz
+#tar -xvf WebGrabPlus_V2.1_install.tar.gz
+#rm -f WebGrabPlus_V2.1_install.tar.gz
+#beta
+wget "http://webgrabplus.com/sites/default/files/download/SW/V2.1.10/WebGrabPlus_V2.1.10_beta_install.tar.gz" -O "WebGrabPlus_V2.1.10_beta_install.tar.gz"
+tar -xvf "WebGrabPlus_V2.1.10_beta_install.tar.gz"
+rm -f "WebGrabPlus_V2.1.10_beta_install.tar.gz"
 rm -rf TF1
 mv .wg++ TF1
 cd TF1
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
 ./install.sh
-rm -f "siteini.user/France/telerama.fr.channels.xml" "siteini.user/France/telerama.fr.ini"
-mkdir -p "siteini.user/France"
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/telerama.fr_.channels.xml" -O "siteini.user/France/telerama.fr.channels.xml"
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/telerama.fr_.ini" -O "siteini.user/France/telerama.fr.ini"
+rm -f "siteini.user/" "siteini.pack/"
+wget "http://webgrabplus.com/sites/default/files/download/ini/SiteIniPack_current.zip" -O "SiteIniPack_current.zip"
+unzip SiteIniPack_current.zip
+rm -f SiteIniPack_current.zip
+wget "http://www.webgrabplus.com/sites/default/files/download/ini/info/zip/France_telerama-sha1.sh.zip" -O "France_telerama-sha1.sh.zip"
+unzip "France_telerama-sha1.sh.zip"
+rm -f "France_telerama-sha1.sh.zip"
 wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1.sh -O /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.sh
 chmod +x /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.sh
 if ! grep -q "00 05 * * * root /bin/bash /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.sh" /etc/crontab; then
