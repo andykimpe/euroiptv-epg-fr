91b26662414999cccb7f2f341f8a6dc3dc61e13c
Kimpe Andy <andykimpe@gmail.com>
2019-07-27 00:13:21 +0200

    Update TF1update.sh

--- config/France/TF1/TF1update.sh
+++ config/France/TF1/TF1update.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-cd /root
 cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1
-rm -rf "WebGrab++.config.xml" "guide.xml" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml.gz
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1.xml" -O "WebGrab++.config.xml"
-rm -f /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml
-./run.sh
+rm -rf "WebGrab++.config.xml" "TF1.xml" "TF1.xml.gz"
+wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1TELERAMACONFIG.conf" -O "TF1TELERAMACONFIG.conf"
+tv_grab_fr_telerama.py --config-file TF1TELERAMACONFIG.conf --output TF1.xml --days 13
 cp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml.save
 gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml
 mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml.save /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml
@@ -16,6 +14,7 @@ sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml > /
 sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml
 sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml
 head -n -1 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml
+sed -i 's|192.tv.telerama.fr|TF1.fr|' "/home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.xml"
 chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/*
 chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/*
 if [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
