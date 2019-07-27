29ae63c8e98425a4a01ab688c3dd9c3d5ee9e3a0
Kimpe Andy <andykimpe@gmail.com>
2019-07-27 04:36:15 +0200

    Update France2update.sh

--- config/France/France2/France2update.sh
+++ config/France/France2/France2update.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-cd /root
 cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2
-rm -rf "WebGrab++.config.xml" "guide.xml" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.gz
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France2/France2.xml" -O "WebGrab++.config.xml"
-rm -f /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml
-./run.sh
+rm -rf "France2TELERAMACONFIG.conf" "France2.xml" "France2.xml.gz"
+wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France2/France2TELERAMACONFIG.conf" -O "France2TELERAMACONFIG.conf"
+tv_grab_fr_telerama.py --config-file France2TELERAMACONFIG.conf --output France2.xml --days 13
 cp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.save
-rm -f /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.gz
 gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml
 mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.save /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml
 sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml
@@ -17,6 +14,7 @@ sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2
 sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml
 sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml
 head -n -1 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml
+sed -i 's|192.tv.telerama.fr|France2.fr|' "/home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml"
 chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/*
 chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/*
 if [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
