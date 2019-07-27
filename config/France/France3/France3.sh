20f4fc8ce7e61529e4a67365b189da7d3ec9dfff
Kimpe Andy <andykimpe@gmail.com>
2019-07-27 04:56:04 +0200

    Update France3.sh

--- config/France/France3/France3.sh
+++ config/France/France3/France3.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 cd /root
-bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France3/France3update.sh)
+bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France3/France3update.sh)
