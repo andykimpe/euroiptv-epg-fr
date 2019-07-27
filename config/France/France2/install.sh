9526f7ebca0e88dffa8a9a78efbca8c8b759b068
Kimpe Andy <andykimpe@gmail.com>
2019-07-27 04:25:02 +0200

    Update install.sh

--- config/France/France2/install.sh
+++ config/France/France2/install.sh
@@ -16,17 +16,22 @@ cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
 cd France
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
-wget http://webgrabplus.com/sites/default/files/download/SW/V2.1.0/WebGrabPlus_V2.1_install.tar.gz
-tar -xvf WebGrabPlus_V2.1_install.tar.gz
-rm -f WebGrabPlus_V2.1_install.tar.gz
-mv .wg++ France2
+rm -rf France2
+mkdir France2
 cd France2
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
-./install.sh
-rm -f "siteini.user/France/telerama.fr.channels.xml" "siteini.user/France/telerama.fr.ini"
-mkdir -p "siteini.user/France"
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/telerama.fr_.channels.xml" -O "siteini.user/France/telerama.fr.channels.xml"
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/telerama.fr_.ini" -O "siteini.user/France/telerama.fr.ini"
+apt-get -y install python python-minimal python2.7-minimal python3-minimal python3.6-minimal
+apt-get -y install python-pip python3-pip
+apt-get -y install python-lxml python3-lxml
+rm -f /usr/bin/python
+ln -s /usr/bin/python3.6 /usr/bin/python
+rm -f /usr/bin/pip
+ln -s /usr/bin/pip3 /usr/bin/pip
+pip install pytz
+apt-get -y install python-html2text python3-html2text
+wget https://github.com/melmorabity/tv_grab_fr_telerama/raw/master/tv_grab_fr_telerama.py -O /usr/bin/tv_grab_fr_telerama.py
+chmod +x /usr/bin/tv_grab_fr_telerama.py
+wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France2/France2TELERAMACONFIG.conf
 wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France2/France2.sh -O /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.sh
 chmod +x /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.sh
 if ! grep -q "00 05 * * * root /bin/bash /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.sh" /etc/crontab; then
