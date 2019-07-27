0292376bafb75cff2556810cb8d40520ec33927a
Kimpe Andy <andykimpe@gmail.com>
2019-07-27 23:31:38 +0200

    Update and rename config/Deutschland/all/all.sh to config/Deutschland/m.tvtoday.de/m.tvtoday.de.sh

--- config/Deutschland/all/all.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd /root
-bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/Deutschland/all/allupdate.sh)
---
+++ config/Deutschland/m.tvtoday.de/m.tvtoday.de.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+cd /root
+bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/Deutschland/m.tvtoday.de/m.tvtoday.deupdate.sh)
