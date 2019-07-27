b66ae7e26d0328c42b88639c3471a691fccd1811
Kimpe Andy <andykimpe@gmail.com>
2019-07-27 18:21:51 +0200

    Update allupdate.sh

--- config/Deutschland/all/allupdate.sh
+++ config/Deutschland/all/allupdate.sh
@@ -1,1 +1,21 @@
-
+#!/bin/bash
+cd /root
+cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all
+rm -rf "WebGrab++.config.xml" "guide.xml" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.xml.gz
+wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/Deutschland/all/all.xml" -O "WebGrab++.config.xml"
+rm -f /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.xml
+./run.sh
+cp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.xml /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.xml.save
+gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France5/France5.xml
+mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.save /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.xml
+chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France5/*
+chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/*
+if [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
+then
+/home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg.php
+elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php ]
+then
+/home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php
+fi
+bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh)
+cp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.xml /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland.xml
