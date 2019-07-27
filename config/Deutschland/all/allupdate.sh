67488ea0fc087f8965fc8ab8abc3e764dd6b3710
Kimpe Andy <andykimpe@gmail.com>
2019-07-27 18:28:04 +0200

    Update allupdate.sh

--- config/Deutschland/all/allupdate.sh
+++ config/Deutschland/all/allupdate.sh
@@ -8,7 +8,7 @@ rm -f /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.xml
 cp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.xml /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.xml.save
 gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.xml
 mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.save /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/all.xml
-chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France5/*
+chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/*
 chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/Deutschland/all/*
 if [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
 then
