814aad621e61c1d7af52d6d34c4ac20e28844d8d
Kimpe Andy <andykimpe@gmail.com>
2019-07-28 04:04:12 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -4,6 +4,6 @@ rm -f /etc/cron.d/purgeiptables
 rm -f /etc/cron.d/TF1
 rm -f /etc/cron.d/France2
 rm -f /etc/cron.d/France3
-rm -f /etc/cron.d/purgeiptables
+rm -f /etc/cron.d/mtvtodayde
 service cron restart
 rm -rf /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/ /usr/bin/purgeiptables
