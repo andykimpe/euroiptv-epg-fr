36a20e7e64d22cc1af8e80d99bbaf75658557c9f
Kimpe Andy <andykimpe@gmail.com>
2019-07-28 03:41:19 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 rm -f /etc/cron.d/genupdate
+rm -f /etc/cron.d/purgeiptables
+rm -f /etc/cron.d/purgeiptables
+rm -f /etc/cron.d/purgeiptables
+rm -f /etc/cron.d/purgeiptables
+rm -f /etc/cron.d/purgeiptables
+rm -f /etc/cron.d/purgeiptables
+rm -f /etc/cron.d/purgeiptables
+rm -f /etc/cron.d/purgeiptables
+rm -f /etc/cron.d/purgeiptables
 service cron restart
 rm -rf rm -rf /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/ /usr/bin/purgeiptables
