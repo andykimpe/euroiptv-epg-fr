ec89ec0062369f532016637d3df5a758373c34b4
Kimpe Andy <andykimpe@gmail.com>
2019-07-28 03:56:25 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -2,12 +2,8 @@
 rm -f /etc/cron.d/genupdate
 rm -f /etc/cron.d/purgeiptables
 rm -f /etc/cron.d/TF1
-rm -f /etc/cron.d/purgeiptables
-rm -f /etc/cron.d/purgeiptables
-rm -f /etc/cron.d/purgeiptables
-rm -f /etc/cron.d/purgeiptables
-rm -f /etc/cron.d/purgeiptables
+rm -f /etc/cron.d/France2
 rm -f /etc/cron.d/purgeiptables
 rm -f /etc/cron.d/purgeiptables
 service cron restart
-rm -rf rm -rf /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/ /usr/bin/purgeiptables
+rm -rf /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/ /usr/bin/purgeiptables
