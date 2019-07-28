00718679184625d41ae283435564ee5d0396a9e2
Kimpe Andy <andykimpe@gmail.com>
2019-07-28 03:29:11 +0200

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-rm -f /etc/crontab
-mv /etc/crontab.xtreamcodesinstallepg /etc/crontab
+rm -f /etc/cron.d/genupdate
 service cron restart
 rm -rf rm -rf /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/ /usr/bin/purgeiptables
