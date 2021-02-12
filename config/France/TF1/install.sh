92280b7a77fbf0d4053501b4a6cb163179f42d1b
Kimpe Andy <andykimpe@gmail.com>
2021-02-12 18:37:33 +0100

    Update install.sh

--- config/France/TF1/install.sh
+++ config/France/TF1/install.sh
@@ -4,6 +4,16 @@
 # Ubuntu server 12.04/14.04/16.04/18.04
 # 32bit and 64bit
 EPG_INSTALLER_VERSION="1.3"
+if [ -d "/home/streamcreed" ];then
+xtreamcodes="ok"
+wwwdir="/home/streamcreed/wwwdir"
+crondir="cronstreamcreed"
+fi
+if [ -d "/home/xtreamcodes/iptv_xtream_codes" ];then
+xtreamcodes="ok"
+wwwdir="/home/xtreamcodes/iptv_xtream_codes/wwwdir"
+crondir="cronstreamcreed"
+fi
 #--- Display the 'welcome' splash/user warning info..
 echo ""
 echo "############################################################"
@@ -11,8 +21,8 @@ echo "#          Welcome to the epg generator Installer          #"
 echo "############################################################"
 sleep 5
 cd /root
-mkdir -p /home/streamcreed/wwwdir/xmltv/France
-cd /home/streamcreed/wwwdir/xmltv/
+mkdir -p $wwwdir/xmltv/France
+cd $wwwdir/xmltv/
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
 cd France
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
@@ -20,9 +30,8 @@ rm -rf TF1
 mkdir TF1
 cd TF1
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
-wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1.sh -O /home/streamcreed/wwwdir/xmltv/France/TF1/TF1.sh
-chmod +x /home/streamcreed/wwwdir/xmltv/France/TF1/TF1.sh
-wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/cron/TF1 -O /etc/cron.d/TF1
+wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1.sh -O $wwwdir/xmltv/France/TF1/TF1.sh
+chmod +x $wwwdir/xmltv/France/TF1/TF1.sh
+wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/$crondir/TF1 -O /etc/cron.d/TF1
 chmod 644 /etc/cron.d/TF1
-service cron restart
-/home/streamcreed/wwwdir/xmltv/France/TF1/TF1.sh
+$wwwdir/xmltv/France/TF1/TF1.sh
