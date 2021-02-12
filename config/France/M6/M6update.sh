62bf3570a652e5155b8211280ad17bd544741635
Kimpe Andy <andykimpe@gmail.com>
2021-02-13 00:07:00 +0100

    Update M6update.sh

--- config/France/M6/M6update.sh
+++ config/France/M6/M6update.sh
@@ -1,28 +1,59 @@
 #!/bin/bash
-cd /root
-cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6
-rm -rf "WebGrab++.config.xml" "guide.xml" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.gz
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/M6/M6.xml" -O "WebGrab++.config.xml"
-rm -f /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml
+if [ -d "/home/streamcreed" ];then
+xtreamcodes="ok"
+wwwdir="/home/streamcreed/wwwdir"
+crondir="cronstreamcreed"
+fi
+if [ -d "/home/xtreamcodes/iptv_xtream_codes" ];then
+xtreamcodes="ok"
+wwwdir="/home/xtreamcodes/iptv_xtream_codes/wwwdir"
+crondir="cronstreamcreed"
+fi
+cd $wwwdir/xmltv/France/M6
+rm -rf *
+wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
+wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/M6/M6.sh -O $wwwdir/xmltv/France/M6/M6.sh
+chmod +x $wwwdir/xmltv/France/M6/M6.sh
+#wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/M6/M6TELERAMACONFIG.conf" -O "M6TELERAMACONFIG.conf"
+#tv_grab_fr_telerama.py --config-file M6TELERAMACONFIG.conf --output M6.xml --days 13
+wget http://webgrabplus.com/sites/default/files/download/SW/V3.1.0/WebGrabPlus_V3.1_install.tar.gz
+tar -xvf WebGrabPlus_V3.1_install.tar.gz
+rm -f WebGrabPlus_V3.1_install.tar.gz
+mv .wg++/* ./
+rm -f .wg++/
+chmod +x install.sh
+./install.sh
+rm -rf siteini.pack
+wget http://webgrabplus.com/sites/default/files/download/ini/SiteIni.Pack_2021.02.11_231100.zip
+unzip SiteIni.Pack_2021.02.11_231100.zip
+rm -f WebGrab++.config.xml
+wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/M6/M6.xml -O $wwwdir/xmltv/France/M6/WebGrab++.config.xml
 ./run.sh
-cp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.save
-gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml
-mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.save /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml
-head -n -1 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml
-chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/*
-chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/*
-if [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
+cp $wwwdir/xmltv/France/M6/M6.xml $wwwdir/xmltv/France/M6/M6.xml.save
+gzip $wwwdir/xmltv/France/M6/M6.xml
+mv $wwwdir/xmltv/France/M6/M6.xml.save $wwwdir/xmltv/France/M6/M6.xml
+sed '1d' $wwwdir/xmltv/France/M6/M6.xml > $wwwdir/xmltv/France/M6/M6.xml.tmp && mv $wwwdir/xmltv/France/M6/M6.xml.tmp $wwwdir/xmltv/France/M6/M6.xml
+sed '1d' $wwwdir/xmltv/France/M6/M6.xml > $wwwdir/xmltv/France/M6/M6.xml.tmp && mv $wwwdir/xmltv/France/M6/M6.xml.tmp $wwwdir/xmltv/France/M6/M6.xml
+sed '1d' $wwwdir/xmltv/France/M6/M6.xml > $wwwdir/xmltv/France/M6/M6.xml.tmp && mv $wwwdir/xmltv/France/M6/M6.xml.tmp $wwwdir/xmltv/France/M6/M6.xml
+sed '1d' $wwwdir/xmltv/France/M6/M6.xml > $wwwdir/xmltv/France/M6/M6.xml.tmp && mv $wwwdir/xmltv/France/M6/M6.xml.tmp $wwwdir/xmltv/France/M6/M6.xml
+sed '1d' $wwwdir/xmltv/France/M6/M6.xml > $wwwdir/xmltv/France/M6/M6.xml.tmp && mv $wwwdir/xmltv/France/M6/M6.xml.tmp $wwwdir/xmltv/France/M6/M6.xml
+sed '1d' $wwwdir/xmltv/France/M6/M6.xml > $wwwdir/xmltv/France/M6/M6.xml.tmp && mv $wwwdir/xmltv/France/M6/M6.xml.tmp $wwwdir/xmltv/France/M6/M6.xml
+#sed '1d' /home/streamcreed/wwwdir/xmltv/France/M6/M6.xml > /home/streamcreed/wwwdir/xmltv/France/M6/M6.xml.tmp && mv /home/streamcreed/wwwdir/xmltv/France/M6/M6.xml.tmp /home/streamcreed/wwwdir/xmltv/France/M6/M6.xml
+head -n -1 $wwwdir/xmltv/France/M6/M6.xml > $wwwdir/xmltv/France/M6/M6.xml.tmp && mv $wwwdir/xmltv/France/M6/M6.xml.tmp $wwwdir/xmltv/France/M6/M6.xml
+#sed -i 's|192.tv.telerama.fr|M6.fr|' "/home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/M6/M6.xml"
+chmod -R 777 $wwwdir/xmltv/France/M6/*
+chown streamcreed:streamcreed $wwwdir/xmltv/France/M6/*
+if [ -f /home/streamcreed/crons/epg.php ]
+then
+/home/streamcreed/php/bin/php /home/streamcreed/crons/epg.php
+elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
 then
 /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg.php
+elif [ -f /home/streamcreed/crons/epg_checking.php ]
+then
+/home/streamcreed/php/bin/php /home/streamcreed/crons/epg_checking.php
 elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php ]
 then
 /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php
 fi
-bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh)
+bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh)
