3a78da462dc47321f0e48890a7ec2cd2ae27fc0c
Kimpe Andy <andykimpe@gmail.com>
2021-02-12 13:57:46 +0100

    Update install.sh

--- config/France/TF1/install.sh
+++ config/France/TF1/install.sh
@@ -3,7 +3,7 @@
 # Supported Operating Systems: 
 # Ubuntu server 12.04/14.04/16.04/18.04
 # 32bit and 64bit
-EPG_INSTALLER_VERSION="1.2"
+EPG_INSTALLER_VERSION="1.3"
 #--- Display the 'welcome' splash/user warning info..
 echo ""
 echo "############################################################"
@@ -11,8 +11,8 @@ echo "#          Welcome to the epg generator Installer          #"
 echo "############################################################"
 sleep 5
 cd /root
-mkdir -p /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France
-cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/
+mkdir -p /home/streamcreed/wwwdir/xmltv/France
+cd /home/streamcreed/wwwdir/xmltv/
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
 cd France
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
@@ -29,12 +29,12 @@ rm -f /usr/bin/pip
 ln -s /usr/bin/pip3 /usr/bin/pip
 pip install pytz
 apt-get -y install python-html2text python3-html2text
-wget https://github.com/melmorabity/tv_grab_fr_telerama/raw/master/tv_grab_fr_telerama.py -O /usr/bin/tv_grab_fr_telerama.py
-chmod +x /usr/bin/tv_grab_fr_telerama.py
+#wget https://github.com/melmorabity/tv_grab_fr_telerama/raw/master/tv_grab_fr_telerama.py -O /usr/bin/tv_grab_fr_telerama.py
+#chmod +x /usr/bin/tv_grab_fr_telerama.py
 wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1TELERAMACONFIG.conf
-wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1.sh -O /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.sh
-chmod +x /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.sh
+wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1.sh -O /home/streamcreed/wwwdir/xmltv/France/TF1/TF1.sh
+chmod +x /home/streamcreed/wwwdir/xmltv/France/TF1/TF1.sh
 wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/cron/TF1 -O /etc/cron.d/TF1
 chmod 644 /etc/cron.d/TF1
 service cron restart
-/home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/TF1/TF1.sh
+/home/streamcreed/wwwdir/xmltv/France/TF1/TF1.sh
