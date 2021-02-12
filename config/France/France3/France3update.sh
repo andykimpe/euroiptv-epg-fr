93e28653707097b67f1ead44a03078e120ffbb0a
Kimpe Andy <andykimpe@gmail.com>
2021-02-12 22:23:15 +0100

    Update France3update.sh

--- config/France/France3/France3update.sh
+++ config/France/France3/France3update.sh
@@ -1,25 +1,57 @@
 #!/bin/bash
-cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3
-rm -rf "France3TELERAMACONFIG.conf" "France3.xml" "France3.xml.gz"
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France3/France3TELERAMACONFIG.conf" -O "France3TELERAMACONFIG.conf"
-tv_grab_fr_telerama.py --config-file France3TELERAMACONFIG.conf --output France3.xml --days 13
-cp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.save
-gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml
-mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.save /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml
-head -n -1 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml
-sed -i 's|80.tv.telerama.fr|France3.fr|' "/home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml"
-chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/*
-chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/*
-if [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
+if [ -d "/home/streamcreed" ];then
+xtreamcodes="ok"
+wwwdir="/home/streamcreed/wwwdir"
+crondir="cronstreamcreed"
+fi
+if [ -d "/home/xtreamcodes/iptv_xtream_codes" ];then
+xtreamcodes="ok"
+wwwdir="/home/xtreamcodes/iptv_xtream_codes/wwwdir"
+crondir="cronstreamcreed"
+fi
+cd $wwwdir/xmltv/France/France3
+rm -rf *
+wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
+wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France3/France3.sh -O $wwwdir/xmltv/France/France3/France3.sh
+chmod +x $wwwdir/xmltv/France/France3/France3.sh
+#wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France3/France3TELERAMACONFIG.conf" -O "France3TELERAMACONFIG.conf"
+#tv_grab_fr_telerama.py --config-file France3TELERAMACONFIG.conf --output France3.xml --days 13
+wget http://webgrabplus.com/sites/default/files/download/SW/V3.1.0/WebGrabPlus_V3.1_install.tar.gz
+tar -xvf WebGrabPlus_V3.1_install.tar.gz
+rm -f WebGrabPlus_V3.1_install.tar.gz
+mv .wg++/* ./
+rm -f .wg++/
+chmod +x install.sh
+./install.sh
+rm -rf siteini.pack
+wget http://webgrabplus.com/sites/default/files/download/ini/SiteIni.Pack_2021.02.11_231100.zip
+unzip SiteIni.Pack_2021.02.11_231100.zip
+rm -f WebGrab++.config.xml
+wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France3/France3.xml -O $wwwdir/xmltv/France/France3/WebGrab++.config.xml
+./run.sh
+cp $wwwdir/xmltv/France/France3/France3.xml $wwwdir/xmltv/France/France3/France3.xml.save
+gzip $wwwdir/xmltv/France/France3/France3.xml
+mv $wwwdir/xmltv/France/France3/France3.xml.save $wwwdir/xmltv/France/France3/France3.xml
+sed '1d' $wwwdir/xmltv/France/France3/France3.xml > $wwwdir/xmltv/France/France3/France3.xml.tmp && mv $wwwdir/xmltv/France/France3/France3.xml.tmp $wwwdir/xmltv/France/France3/France3.xml
+sed '1d' $wwwdir/xmltv/France/France3/France3.xml > $wwwdir/xmltv/France/France3/France3.xml.tmp && mv $wwwdir/xmltv/France/France3/France3.xml.tmp $wwwdir/xmltv/France/France3/France3.xml
+sed '1d' $wwwdir/xmltv/France/France3/France3.xml > $wwwdir/xmltv/France/France3/France3.xml.tmp && mv $wwwdir/xmltv/France/France3/France3.xml.tmp $wwwdir/xmltv/France/France3/France3.xml
+sed '1d' $wwwdir/xmltv/France/France3/France3.xml > $wwwdir/xmltv/France/France3/France3.xml.tmp && mv $wwwdir/xmltv/France/France3/France3.xml.tmp $wwwdir/xmltv/France/France3/France3.xml
+sed '1d' $wwwdir/xmltv/France/France3/France3.xml > $wwwdir/xmltv/France/France3/France3.xml.tmp && mv $wwwdir/xmltv/France/France3/France3.xml.tmp $wwwdir/xmltv/France/France3/France3.xml
+sed '1d' $wwwdir/xmltv/France/France3/France3.xml > $wwwdir/xmltv/France/France3/France3.xml.tmp && mv $wwwdir/xmltv/France/France3/France3.xml.tmp $wwwdir/xmltv/France/France3/France3.xml
+#sed '1d' /home/streamcreed/wwwdir/xmltv/France/France3/France3.xml > /home/streamcreed/wwwdir/xmltv/France/France3/France3.xml.tmp && mv /home/streamcreed/wwwdir/xmltv/France/France3/France3.xml.tmp /home/streamcreed/wwwdir/xmltv/France/France3/France3.xml
+head -n -1 $wwwdir/xmltv/France/France3/France3.xml > $wwwdir/xmltv/France/France3/France3.xml.tmp && mv $wwwdir/xmltv/France/France3/France3.xml.tmp $wwwdir/xmltv/France/France3/France3.xml
+#sed -i 's|192.tv.telerama.fr|France3.fr|' "/home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France3/France3.xml"
+chmod -R 777 $wwwdir/xmltv/France/France3/*
+chown streamcreed:streamcreed $wwwdir/xmltv/France/France3/*
+if [ -f /home/streamcreed/crons/epg.php ]
+then
+/home/streamcreed/php/bin/php /home/streamcreed/crons/epg.php
+elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
 then
 /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg.php
+elif [ -f /home/streamcreed/crons/epg_checking.php ]
+then
+/home/streamcreed/php/bin/php /home/streamcreed/crons/epg_checking.php
 elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php ]
 then
 /home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php
