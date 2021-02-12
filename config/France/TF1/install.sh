c65297a26ba3ac9d78c3075290c80acf2fa5a743
Kimpe Andy <andykimpe@gmail.com>
2021-02-12 16:38:41 +0100

    Update install.sh

--- config/France/TF1/install.sh
+++ config/France/TF1/install.sh
@@ -20,18 +20,6 @@ rm -rf TF1
 mkdir TF1
 cd TF1
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
-apt-get -y install python python-minimal python2.7-minimal python3-minimal python3.6-minimal
-apt-get -y install python-pip python3-pip
-apt-get -y install python-lxml python3-lxml
-rm -f /usr/bin/python
-ln -s /usr/bin/python3.6 /usr/bin/python
-rm -f /usr/bin/pip
-ln -s /usr/bin/pip3 /usr/bin/pip
-pip install pytz
-apt-get -y install python-html2text python3-html2text
-#wget https://github.com/melmorabity/tv_grab_fr_telerama/raw/master/tv_grab_fr_telerama.py -O /usr/bin/tv_grab_fr_telerama.py
-#chmod +x /usr/bin/tv_grab_fr_telerama.py
-#wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1TELERAMACONFIG.conf
 wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/TF1.sh -O /home/streamcreed/wwwdir/xmltv/France/TF1/TF1.sh
 chmod +x /home/streamcreed/wwwdir/xmltv/France/TF1/TF1.sh
 wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/cron/TF1 -O /etc/cron.d/TF1
