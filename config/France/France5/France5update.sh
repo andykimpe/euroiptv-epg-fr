d92bfdac538a3b4720debad6fa81a60f25d914bb
Kimpe Andy <andykimpe@gmail.com>
2021-02-13 00:58:16 +0100

    Update France5update.sh

--- config/France/France5/France5update.sh
+++ config/France/France5/France5update.sh
@@ -14,18 +14,17 @@ rm -rf *
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
 wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France5/France5.sh -O $wwwdir/xmltv/France/France5/France5.sh
 chmod +x $wwwdir/xmltv/France/France5/France5.sh
-#wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France5/France5TELERAMACONFIG.conf" -O "France5TELERAMACONFIG.conf"
-#tv_grab_fr_telerama.py --config-file France5TELERAMACONFIG.conf --output France5.xml --days 13
-wget http://webgrabplus.com/sites/default/files/download/SW/V3.1.0/WebGrabPlus_V3.1_install.tar.gz
-tar -xvf WebGrabPlus_V3.1_install.tar.gz
-rm -f WebGrabPlus_V3.1_install.tar.gz
+wget `wget -qO- https://raw.githubusercontent.com/andykimpe/euroiptv-epg-fr/master/webgrabplusplusinstallurl`
+tar -xvf *.tar.gz
+rm -f *.tar.gz
 mv .wg++/* ./
 rm -f .wg++/
 chmod +x install.sh
 ./install.sh
 rm -rf siteini.pack
-wget http://webgrabplus.com/sites/default/files/download/ini/SiteIni.Pack_2021.02.11_231100.zip
-unzip SiteIni.Pack_2021.02.11_231100.zip
+wget `wget -qO- https://raw.githubusercontent.com/andykimpe/euroiptv-epg-fr/master/webgrabplusplussiteiniurl`
+unzip *.zip
+rm -f *.zip
 rm -f WebGrab++.config.xml
 wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France5/France5.xml -O $wwwdir/xmltv/France/France5/WebGrab++.config.xml
 ./run.sh
