24c56ee0c17ba50f7f47192e06d31ce5c7804ac3
Kimpe Andy <andykimpe@gmail.com>
2021-02-12 14:32:09 +0100

    Update gen.sh

--- gen.sh
+++ gen.sh
@@ -7,20 +7,20 @@ cat > /home/streamcreed/wwwdir/xmltv/guide.xml <<EOF
 EOF
 cat >> /home/streamcreed/wwwdir/xmltv/guide.xml <<EOF
   <channel id="TF1.fr">
-    <display-name lang="fr">FR TF1</display-name>
+    <display-name lang="fr">|FR| TF1</display-name>
     <icon src="https://github.com/andykimpe/euroiptv-epg-fr/raw/master/logo/France/tf1.png" />
     <url>https://github.com/andykimpe/euroiptv-epg-fr</url>
   </channel>
 EOF
 cat /home/streamcreed/wwwdir/xmltv/France/TF1/TF1.xml >> /home/streamcreed/wwwdir/xmltv/guide.xml
-#cat >> /home/streamcreed/wwwdir/xmltv/guide.xml <<EOF
-#  <channel id="France2.fr">
-#    <display-name lang="fr"> FR France 2</display-name>
-#    <icon src="https://github.com/andykimpe/euroiptv-epg-fr/raw/master/logo/France/france2.png" />
-#    <url>https://github.com/andykimpe/euroiptv-epg-fr/</url>
-#  </channel>
-#EOF
-#cat /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml >> /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml
+cat >> /home/streamcreed/wwwdir/xmltv/guide.xml <<EOF
+  <channel id="France2.fr">
+    <display-name lang="fr">|FR| France 2</display-name>
+    <icon src="https://github.com/andykimpe/euroiptv-epg-fr/raw/master/logo/France/france2.png" />
+    <url>https://github.com/andykimpe/euroiptv-epg-fr/</url>
+  </channel>
+EOF
+cat /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/France2/France2.xml >> /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml
 #cat >> /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/guide.xml <<EOF
 #  <channel id="France3.fr">
 #    <display-name lang="fr">FR France 3</display-name>
