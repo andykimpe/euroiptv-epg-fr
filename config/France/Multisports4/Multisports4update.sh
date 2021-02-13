2c626c5142e80fd4a1c7c5af3c614b9d05dc1741
Kimpe Andy <andykimpe@gmail.com>
2021-02-13 04:31:28 +0100

    Update Multisports4update.sh

--- config/France/Multisports4/Multisports4update.sh
+++ config/France/Multisports4/Multisports4update.sh
@@ -31,7 +31,11 @@ unzip *.zip
 rm -f *.zip
 rm -f WebGrab++.config.xml
 wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/Multisports4/Multisports4.xml -O $wwwdir/xmltv/France/Multisports4/WebGrab++.config.xml
-./run.sh
+if [ -f "/usr/bin/mono" ]; then
+    mono $wwwdir/xmltv/France/Multisports4/bin/WebGrab+Plus.exe $wwwdir/xmltv/France/Multisports4
+else
+$wwwdir/xmltv/France/Multisports4/bin/WebGrab+Plus.exe $wwwdir/xmltv/France/Multisports4
+fi
 cp $wwwdir/xmltv/France/Multisports4/Multisports4.xml $wwwdir/xmltv/France/Multisports4/Multisports4.xml.save
 gzip $wwwdir/xmltv/France/Multisports4/Multisports4.xml
 mv $wwwdir/xmltv/France/Multisports4/Multisports4.xml.save $wwwdir/xmltv/France/Multisports4/Multisports4.xml
