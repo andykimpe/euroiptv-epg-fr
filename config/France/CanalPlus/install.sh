8086e1d56fff6aa30631ef0b42ee0474b3068dcc
Kimpe Andy <andykimpe@gmail.com>
2021-02-13 12:43:38 +0100

    Update install.sh

--- config/France/CanalPlus/install.sh
+++ config/France/CanalPlus/install.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-# installateur pour epg de euroiptv
-# Supported Operating Systems: 
-# Ubuntu server 12.04/14.04/16.04/18.04
-# 32bit and 64bit
-EPG_INSTALLER_VERSION="1.3"
-if [ -d "/home/streamcreed" ];then
+if [ -d "C:/cygwin64/home/streamcreed" ];then
+xtreamcodes="ok"
+wwwdir="C:/cygwin64/home/streamcreed/wwwdir"
+crondir="cronstreamcreed"
+elif [ -d "/home/streamcreed" ];then
 xtreamcodes="ok"
 wwwdir="/home/streamcreed/wwwdir"
 crondir="cronstreamcreed"
@@ -14,24 +13,7 @@ xtreamcodes="ok"
 wwwdir="/home/xtreamcodes/iptv_xtream_codes/wwwdir"
 crondir="cronstreamcreed"
 fi
-#--- Display the 'welcome' splash/user warning info..
-echo ""
-echo "############################################################"
-echo "#          Welcome to the epg generator Installer          #"
-echo "############################################################"
-sleep 5
-cd /root
-mkdir -p $wwwdir/xmltv/France
 cd $wwwdir/xmltv/
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
-cd France
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
-rm -rf CanalPlus
-mkdir CanalPlus
-cd CanalPlus
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
-wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/CanalPlus/CanalPlus.sh -O $wwwdir/xmltv/France/CanalPlus/CanalPlus.sh
-chmod +x $wwwdir/xmltv/France/CanalPlus/CanalPlus.sh
-wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/CanalPlus/$crondir/CanalPlus -O /etc/cron.d/CanalPlus
-chmod 644 /etc/cron.d/CanalPlus
-$wwwdir/xmltv/France/CanalPlus/CanalPlus.sh
+wget https://raw.githubusercontent.com/andykimpe/euroiptv-epg-fr/master/installchannel.sh -O $wwwdir/xmltv/installchannel.sh
+chmod +x $wwwdir/xmltv/installchannel.sh
+$wwwdir/xmltv/installchannel.sh France CanalPlus
