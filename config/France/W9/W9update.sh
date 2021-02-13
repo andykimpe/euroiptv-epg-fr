c6b8c92dc2a64560a6b191fe2b8b9ee4df9b7f0a
Kimpe Andy <andykimpe@gmail.com>
2021-02-13 14:12:11 +0100

    Update W9update.sh

--- config/France/W9/W9update.sh
+++ config/France/W9/W9update.sh
@@ -1,28 +1,19 @@
 #!/bin/bash
-cd /root
-cd /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9
-rm -rf "WebGrab++.config.xml" "guide.xml" /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.gz
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/W9/W9.xml" -O "WebGrab++.config.xml"
-rm -f /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml
-./run.sh
-cp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.save
-gzip /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml
-mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.save /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml
-sed '1d' /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml
-head -n -1 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml > /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp && mv /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml.tmp /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/W9.xml
-chmod -R 777 /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/*
-chown xtreamcodes:xtreamcodes /home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/W9/*
-if [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
-then
-/home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg.php
-elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php ]
-then
-/home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php
+if [ -d "C:/cygwin64/home/streamcreed" ];then
+xtreamcodes="ok"
+wwwdir="C:/cygwin64/home/streamcreed/wwwdir"
+crondir="cronstreamcreed"
+elif [ -d "/home/streamcreed" ];then
+xtreamcodes="ok"
+wwwdir="/home/streamcreed/wwwdir"
+crondir="cronstreamcreed"
 fi
-bash <(curl -L -Ss https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh)
+if [ -d "/home/xtreamcodes/iptv_xtream_codes" ];then
+xtreamcodes="ok"
+wwwdir="/home/xtreamcodes/iptv_xtream_codes/wwwdir"
+crondir="cronstreamcreed"
+fi
+cd $wwwdir/xmltv/
+wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/updatechannel.sh -O $wwwdir/xmltv/updatechannel.sh
+chmod +x $wwwdir/xmltv/updatechannel.sh
+$wwwdir/xmltv/updatechannel.sh France W9
