fbd81fd4d84b677791c62788f1f46600870f61f1
Kimpe Andy <andykimpe@gmail.com>
2021-02-13 13:29:58 +0100

    Update Multisports6update.sh

--- config/France/Multisports6/Multisports6update.sh
+++ config/France/Multisports6/Multisports6update.sh
@@ -13,54 +13,7 @@ xtreamcodes="ok"
 wwwdir="/home/xtreamcodes/iptv_xtream_codes/wwwdir"
 crondir="cronstreamcreed"
 fi
-cd $wwwdir/xmltv/France/Multisports6
-rm -rf *
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
-wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/Multisports6/Multisports6.sh -O $wwwdir/xmltv/France/Multisports6/Multisports6.sh
-chmod +x $wwwdir/xmltv/France/Multisports6/Multisports6.sh
-wget `wget -qO- https://raw.githubusercontent.com/andykimpe/euroiptv-epg-fr/master/webgrabplusplusinstallurl`
-tar -xvf *.tar.gz
-rm -f *.tar.gz
-mv .wg++/* ./
-rm -f .wg++/
-chmod +x install.sh
-./install.sh
-rm -rf siteini.pack
-wget `wget -qO- https://raw.githubusercontent.com/andykimpe/euroiptv-epg-fr/master/webgrabplusplussiteiniurl`
-unzip *.zip
-rm -f *.zip
-rm -f WebGrab++.config.xml
-wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/Multisports6/Multisports6.xml -O $wwwdir/xmltv/France/Multisports6/WebGrab++.config.xml
-if [ -f "/usr/bin/mono" ]; then
-    mono $wwwdir/xmltv/France/Multisports6/bin/WebGrab+Plus.exe $wwwdir/xmltv/France/Multisports6
-else
-$wwwdir/xmltv/France/Multisports6/bin/WebGrab+Plus.exe $wwwdir/xmltv/France/Multisports6
-fi
-cp $wwwdir/xmltv/France/Multisports6/Multisports6.xml $wwwdir/xmltv/France/Multisports6/Multisports6.xml.save
-gzip $wwwdir/xmltv/France/Multisports6/Multisports6.xml
-mv $wwwdir/xmltv/France/Multisports6/Multisports6.xml.save $wwwdir/xmltv/France/Multisports6/Multisports6.xml
-sed '1d' $wwwdir/xmltv/France/Multisports6/Multisports6.xml > $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp && mv $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp $wwwdir/xmltv/France/Multisports6/Multisports6.xml
-sed '1d' $wwwdir/xmltv/France/Multisports6/Multisports6.xml > $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp && mv $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp $wwwdir/xmltv/France/Multisports6/Multisports6.xml
-sed '1d' $wwwdir/xmltv/France/Multisports6/Multisports6.xml > $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp && mv $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp $wwwdir/xmltv/France/Multisports6/Multisports6.xml
-sed '1d' $wwwdir/xmltv/France/Multisports6/Multisports6.xml > $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp && mv $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp $wwwdir/xmltv/France/Multisports6/Multisports6.xml
-sed '1d' $wwwdir/xmltv/France/Multisports6/Multisports6.xml > $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp && mv $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp $wwwdir/xmltv/France/Multisports6/Multisports6.xml
-sed '1d' $wwwdir/xmltv/France/Multisports6/Multisports6.xml > $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp && mv $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp $wwwdir/xmltv/France/Multisports6/Multisports6.xml
-#sed '1d' /home/streamcreed/wwwdir/xmltv/France/Multisports6/Multisports6.xml > /home/streamcreed/wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp && mv /home/streamcreed/wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp /home/streamcreed/wwwdir/xmltv/France/Multisports6/Multisports6.xml
-head -n -1 $wwwdir/xmltv/France/Multisports6/Multisports6.xml > $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp && mv $wwwdir/xmltv/France/Multisports6/Multisports6.xml.tmp $wwwdir/xmltv/France/Multisports6/Multisports6.xml
-#sed -i 's|192.tv.telerama.fr|Multisports6.fr|' "/home/xtreamcodes/iptv_xtream_codes/wwwdir/xmltv/France/Multisports6/Multisports6.xml"
-chmod -R 777 $wwwdir/xmltv/France/Multisports6/*
-chown streamcreed:streamcreed $wwwdir/xmltv/France/Multisports6/*
-if [ -f /home/streamcreed/crons/epg.php ]
-then
-/home/streamcreed/php/bin/php /home/streamcreed/crons/epg.php
-elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg.php ]
-then
-/home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg.php
-elif [ -f /home/streamcreed/crons/epg_checking.php ]
-then
-/home/streamcreed/php/bin/php /home/streamcreed/crons/epg_checking.php
-elif [ -f /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php ]
-then
-/home/xtreamcodes/iptv_xtream_codes/php/bin/php /home/xtreamcodes/iptv_xtream_codes/crons/epg_checking.php
-fi
-bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh)
+cd $wwwdir/xmltv/
+wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/updatechannel.sh -O $wwwdir/xmltv/updatechannel.sh
+chmod +x $wwwdir/xmltv/updatechannel.sh
+$wwwdir/xmltv/updatechannel.sh France Multisports6
