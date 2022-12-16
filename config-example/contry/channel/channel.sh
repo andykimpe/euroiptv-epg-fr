9a6f865c9eb1b55ba60ad3b8e95bc39c6b11689c
Kimpe Andy <andykimpe@gmail.com>
2022-12-16 21:36:46 +0100

    Update channel.sh

--- config-example/contry/channel/channel.sh
+++ config-example/contry/channel/channel.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
-cd /root
-bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config-example/contry/channel/channelupdate.sh)
+bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/wgcontry/wgchannel/wgchannelupdate.sh)
