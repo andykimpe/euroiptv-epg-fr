d34d08e722fc5de3a915e73b03fb31a996bbe494
Kimpe Andy <andykimpe@gmail.com>
2022-12-17 11:14:42 +0100

    add news channel

--- gen2.sh
+++ gen2.sh
@@ -116,3 +116,11 @@ cat >> $wwwdir/xmltv/guide.xml <<EOF
   </channel>
 EOF
 cat $wwwdir/xmltv/France/LCPPS/LCPPS.xml >> $wwwdir/xmltv/guide.xml
+cat >> $wwwdir/xmltv/guide.xml <<EOF
+  <channel id="France4.fr">
+    <display-name lang="fr">|FR| France 4</display-name>
+    <icon src="https://raw.githubusercontent.com/andykimpe/euroiptv-epg-fr/master/logo/France/france4.png" />
+    <url>https://github.com/andykimpe/euroiptv-epg-fr</url>
+  </channel>
+EOF
+cat $wwwdir/xmltv/France/France4/France4.xml >> $wwwdir/xmltv/guide.xml
