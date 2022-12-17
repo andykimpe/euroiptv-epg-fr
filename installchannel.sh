55d20806438561df933b1381ff88acaeb793225f
Kimpe Andy <andykimpe@gmail.com>
2022-12-17 02:16:00 +0100

    Update installchannel.sh

--- installchannel.sh
+++ installchannel.sh
@@ -4,22 +4,18 @@
 # Ubuntu server 12.04/14.04/16.04/18.04
 # 32bit and 64bit
 EPG_INSTALLER_VERSION="1.3"
-if [ -d "C:/cygwin64/home/streamcreed" ];then
-xtreamcodes="ok"
-wwwdir="C:/cygwin64/home/streamcreed/wwwdir"
-crondir="cronstreamcreed"
-user="nouser"
-elif [ -d "/home/streamcreed" ];then
+if [ -d "/home/streamcreed" ];then
 xtreamcodes="ok"
 wwwdir="/home/streamcreed/wwwdir"
 crondir="cronstreamcreed"
-user="streamcreed"
-fi
-if [ -d "/home/xtreamcodes/iptv_xtream_codes" ];then
+elif [ -d "/home/xtreamcodes/iptv_xtream_codes" ];then
 xtreamcodes="ok"
 wwwdir="/home/xtreamcodes/iptv_xtream_codes/wwwdir"
-crondir="cronstreamcreed"
-user="xtreamcodes"
+crondir="cron"
+else
+xtreamcodes="no"
+wwwdir="/var/www/html"
+crondir="cronapache"
 fi
 #--- Display the 'welcome' splash/user warning info..
 echo ""
@@ -27,7 +23,6 @@ echo "############################################################"
 echo "#          Welcome to the epg generator Installer          #"
 echo "############################################################"
 sleep 5
-cd /root
 mkdir -p $wwwdir/xmltv/$1
 cd $wwwdir/xmltv/
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
@@ -39,6 +34,8 @@ cd $2
 wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
 wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/$1/$2/$2.sh -O $wwwdir/xmltv/$1/$2/$2.sh
 chmod +x $wwwdir/xmltv/$1/$2/$2.sh
+sed -i 's|wgcountry|'$1'|' $wwwdir/xmltv/$1/$2/$2.sh
+sed -i 's|wgchannel|'$2'|' $wwwdir/xmltv/$1/$2/$2.sh
 wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/$1/$2/$crondir/$2 -O /etc/cron.d/$2
 chmod 644 /etc/cron.d/$2
 $wwwdir/xmltv/$1/$2/$2.sh
