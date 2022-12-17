847e1eacdb36cc0fcf47cbd6554b41a76a543599
Kimpe Andy <andykimpe@gmail.com>
2022-12-17 16:51:16 +0100

    add news channel

--- gen2.sh
+++ gen2.sh
@@ -108,3 +108,11 @@ cat >> $wwwdir/xmltv/guide.xml <<EOF
   </channel>
 EOF
 cat $wwwdir/xmltv/France/NRJ12/NRJ12.xml >> $wwwdir/xmltv/guide.xml
+cat >> $wwwdir/xmltv/guide.xml <<EOF
+  <channel id="LCP.fr">
+    <display-name lang="fr">|FR| LCP/Public Senat</display-name>
+    <icon src="https://raw.githubusercontent.com/andykimpe/euroiptv-epg-fr/master/logo/France/lcp-ps.png" />
+    <url>https://github.com/andykimpe/euroiptv-epg-fr</url>
+  </channel>
+EOF
+cat $wwwdir/xmltv/France/LCPPS/LCPPS.xml >> $wwwdir/xmltv/guide.xml
