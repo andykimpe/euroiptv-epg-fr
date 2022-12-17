81edf8e5888c6a1874c0857d1fd9f923fa51a6c9
Kimpe Andy <andykimpe@gmail.com>
2022-12-17 10:41:46 +0100

    Update installchannel.sh

--- installchannel.sh
+++ installchannel.sh
@@ -25,17 +25,17 @@ echo "############################################################"
 sleep 5
 mkdir -p $wwwdir/xmltv/$1
 cd $wwwdir/xmltv/
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
+wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
 cd $1
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
+wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
 rm -rf $2
 mkdir $2
 cd $2
-wget --no-check-certificate "https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
+wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/index.php" -O "index.php"
 wget --no-check-certificate https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/$1/$2/$2.sh -O $wwwdir/xmltv/$1/$2/$2.sh
 chmod +x $wwwdir/xmltv/$1/$2/$2.sh
-sed -i 's|wgcountry|'$1'|' $wwwdir/xmltv/$1/$2/$2.sh
-sed -i 's|wgchannel|'$2'|' $wwwdir/xmltv/$1/$2/$2.sh
+sed -i "s|wgcountry|"$1"|g" $wwwdir/xmltv/$1/$2/$2.sh
+sed -i "s|wgchannel|"$2"|g" $wwwdir/xmltv/$1/$2/$2.sh
 wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/$1/$2/$crondir/$2 -O /etc/cron.d/$2
 chmod 644 /etc/cron.d/$2
 $wwwdir/xmltv/$1/$2/$2.sh
