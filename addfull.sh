8aababc1669bd0d4ee4aa50f718c1aabd9e8dd03
Kimpe Andy <andykimpe@gmail.com>
2022-12-18 13:42:12 +0100

    Update addfull.sh

--- addfull.sh
+++ addfull.sh
@@ -66,3 +66,4 @@ wget https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config-example/inst
 ./addchannel.sh fr France ParamountChannelDecale "|FR| Paramount Channel Decale" sfr.fr "2072" ParamountChannelDecale.fr logo/France/paramountchanneldecale.png
 ./addchannel.sh fr France ParamountChannel "|FR| Paramount Channel" canalsat.fr 487 ParamountChannel.fr logo/France/paramountchannel.png
 ./addchannel.sh fr France Action "|FR| Action" canalsat.fr 504 Action.fr logo/France/action.png
+./addchannel.sh fr France AlticeStudio "|FR| Altice Studio" sfr.fr 2320 AlticeStudio.fr logo/France/alticestudio.png
