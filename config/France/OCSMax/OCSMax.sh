1f4855711bb20458501ea16ad41c5653ee47f61f
Kimpe Andy <andykimpe@gmail.com>
2022-12-18 11:26:45 +0100

    add news channel

---
+++ config/France/OCSMax/OCSMax.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/OCSMax/OCSMaxupdate.sh)
--- gen2.sh
+++ gen2.sh
@@ -308,3 +308,11 @@ cat >> $wwwdir/xmltv/guide.xml <<EOF
   </channel>
 EOF
 cat $wwwdir/xmltv/France/CinePlusClassic/CinePlusClassic.xml >> $wwwdir/xmltv/guide.xml
+cat >> $wwwdir/xmltv/guide.xml <<EOF
+  <channel id="OrangeCinemax.fr">
+    <display-name lang="fr">|FR| OCS Max</display-name>
+    <icon src="https://raw.githubusercontent.com/andykimpe/euroiptv-epg-fr/master/logo/France/ocsmax.png" />
+    <url>https://github.com/andykimpe/euroiptv-epg-fr</url>
+  </channel>
+EOF
+cat $wwwdir/xmltv/France/OCSMax/OCSMax.xml >> $wwwdir/xmltv/guide.xml
