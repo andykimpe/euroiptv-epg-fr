e1ed834c49c5db06beaecb499f068b7af0579ff0
Kimpe Andy <andykimpe@gmail.com>
2022-12-18 11:17:34 +0100

    add news channel

--- gen2.sh
+++ gen2.sh
@@ -300,3 +300,11 @@ cat >> $wwwdir/xmltv/guide.xml <<EOF
   </channel>
 EOF
 cat $wwwdir/xmltv/France/CinePlusClub/CinePlusClub.xml >> $wwwdir/xmltv/guide.xml
+cat >> $wwwdir/xmltv/guide.xml <<EOF
+  <channel id="CinecinemaClassic.fr">
+    <display-name lang="fr">|FR| Cine+ Classic</display-name>
+    <icon src="https://raw.githubusercontent.com/andykimpe/euroiptv-epg-fr/master/logo/France/cineplusclassic.png" />
+    <url>https://github.com/andykimpe/euroiptv-epg-fr</url>
+  </channel>
+EOF
+cat $wwwdir/xmltv/France/CinePlusClassic/CinePlusClassic.xml >> $wwwdir/xmltv/guide.xml
--- installfullchannell.sh
+++ installfullchannell.sh
@@ -36,3 +36,5 @@ bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/
 bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/CinePlusFamiz/install.sh)
 bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/CinePlusClub/install.sh)
 bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh)
+bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/CinePlusClassic/install.sh)
+bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh)
