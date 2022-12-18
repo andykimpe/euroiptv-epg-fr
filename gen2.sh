44080e11992248468059e8685b5f64c294283ce7
Kimpe Andy <andykimpe@gmail.com>
2022-12-18 11:59:39 +0100

    add news channel

--- gen2.sh
+++ gen2.sh
@@ -324,3 +324,11 @@ cat >> $wwwdir/xmltv/guide.xml <<EOF
   </channel>
 EOF
 cat $wwwdir/xmltv/France/OCSCity/OCSCity.xml >> $wwwdir/xmltv/guide.xml
+cat >> $wwwdir/xmltv/guide.xml <<EOF
+  <channel id="OrangeCinechoc.fr">
+    <display-name lang="fr">|FR| OCS Choc</display-name>
+    <icon src="https://raw.githubusercontent.com/andykimpe/euroiptv-epg-fr/master/logo/France/ocschoc.png" />
+    <url>https://github.com/andykimpe/euroiptv-epg-fr</url>
+  </channel>
+EOF
+cat $wwwdir/xmltv/France/OCSChoc/OCSChoc.xml >> $wwwdir/xmltv/guide.xml
