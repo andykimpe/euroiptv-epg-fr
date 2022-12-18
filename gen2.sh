842de18883ea40c5242f837655bfa71a9148f36a
Kimpe Andy <andykimpe@gmail.com>
2022-12-18 12:23:57 +0100

    add news channel

--- gen2.sh
+++ gen2.sh
@@ -340,3 +340,11 @@ cat >> $wwwdir/xmltv/guide.xml <<EOF
   </channel>
 EOF
 cat $wwwdir/xmltv/France/OCSGeants/OCSGeants.xml >> $wwwdir/xmltv/guide.xml
+cat >> $wwwdir/xmltv/guide.xml <<EOF
+  <channel id="TCM.fr">
+    <display-name lang="fr">|FR| TCM Cinema</display-name>
+    <icon src="https://raw.githubusercontent.com/andykimpe/euroiptv-epg-fr/master/logo/France/tcmcinema.png" />
+    <url>https://github.com/andykimpe/euroiptv-epg-fr</url>
+  </channel>
+EOF
+cat $wwwdir/xmltv/France/TCMCinema/TCMCinema.xml >> $wwwdir/xmltv/guide.xml
