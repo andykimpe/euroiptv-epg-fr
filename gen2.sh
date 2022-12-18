351376b1782486076b11a48952098e460145cf5e
Kimpe Andy <andykimpe@gmail.com>
2022-12-18 13:30:45 +0100

    add news channel

--- gen2.sh
+++ gen2.sh
@@ -364,3 +364,11 @@ cat >> $wwwdir/xmltv/guide.xml <<EOF
   </channel>
 EOF
 cat $wwwdir/xmltv/France/ParamountChannel/ParamountChannel.xml >> $wwwdir/xmltv/guide.xml
+cat >> $wwwdir/xmltv/guide.xml <<EOF
+  <channel id="Action.fr">
+    <display-name lang="fr">|FR| Action</display-name>
+    <icon src="https://raw.githubusercontent.com/andykimpe/euroiptv-epg-fr/master/logo/France/action.png" />
+    <url>https://github.com/andykimpe/euroiptv-epg-fr</url>
+  </channel>
+EOF
+cat $wwwdir/xmltv/France/Action/Action.xml >> $wwwdir/xmltv/guide.xml
