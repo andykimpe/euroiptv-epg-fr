6eb2fdc6db2c5faaa49b1aa5f2b7f6f0c60d91f0
Kimpe Andy <andykimpe@gmail.com>
2022-12-19 14:58:33 +0100

    add news channel

--- installfullchannell.sh
+++ installfullchannell.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/TF1/install.sh)
 bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh)
+bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/config/France/France2/install.sh)
+bash <(wget -qO- https://github.com/andykimpe/euroiptv-epg-fr/raw/master/gen.sh)
